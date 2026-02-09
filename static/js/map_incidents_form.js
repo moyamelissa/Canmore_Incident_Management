7ceb6a613d0d6866e432ba220f2410d553bd222a
Moya <melissa.moya@ssc-spc.gc.ca>
2026-02-09 09:47:48 -0500

    Clean CSS files and fix encoding

--- static/js/map_incidents_form.js
+++ static/js/map_incidents_form.js
@@ -68,48 +68,56 @@ window.showIncidentForm = function(map, latlng) {
                 }
                 if (form) {
                     form.onsubmit = function(e) {
-                            e.preventDefault();
-                            var comment = commentArea.value.trim();
-                            var wordCount = comment.split(/\s+/).filter(Boolean).length;
-                            if (wordCount > 150) {
-                                alert('Le commentaire ne doit pas dépasser 150 mots.');
-                                return;
-                            }
-                            // Dépose un marqueur rouge à l'emplacement de l'incident
-                            var redIcon = L.icon({
-                                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
-                                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-                                iconSize: [25, 41],
-                                iconAnchor: [12, 41],
-                                popupAnchor: [1, -34],
-                                shadowSize: [41, 41]
-                            });
-                            var marker = L.marker(latlng, {icon: redIcon}).addTo(map);
-                            marker.bindPopup('<b>Incident signalé</b><br>' +
-                                'Sujet : ' + subjectSelect.options[subjectSelect.selectedIndex].text + '<br>' +
-                                'Détail : ' + detailSelect.options[detailSelect.selectedIndex].text + '<br>' +
-                                'Commentaire : ' + commentArea.value.replace(/</g, '&lt;').replace(/>/g, '&gt;')
-                            ).openPopup();
-                            map.closePopup();
+                        e.preventDefault();
+                        var comment = commentArea.value.trim();
+                        var wordCount = comment.split(/\s+/).filter(Boolean).length;
+                        if (wordCount > 150) {
+                            alert('Le commentaire ne doit pas dépasser 150 mots.');
+                            return;
+                        }
+                        if (!subjectSelect.value) {
+                            alert('Veuillez sélectionner un sujet avant de soumettre le signalement.');
+                            return;
+                        }
+                        if (!detailSelect.value) {
+                            alert('Veuillez sélectionner un détail avant de soumettre le signalement.');
+                            return;
+                        }
+                        // Dépose un marqueur rouge à l'emplacement de l'incident
+                        var redIcon = L.icon({
+                            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+                            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+                            iconSize: [25, 41],
+                            iconAnchor: [12, 41],
+                            popupAnchor: [1, -34],
+                            shadowSize: [41, 41]
+                        });
+                        var marker = L.marker(latlng, {icon: redIcon}).addTo(map);
+                        marker.bindPopup('<b>Incident signalé</b><br>' +
+                            'Sujet : ' + subjectSelect.options[subjectSelect.selectedIndex].text + '<br>' +
+                            'Détail : ' + detailSelect.options[detailSelect.selectedIndex].text + '<br>' +
+                            'Commentaire : ' + commentArea.value.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+                        ).openPopup();
+                        map.closePopup();
 
-                            // Envoie les données de l'incident au backend
-                            var incidentData = {
-                                type: subjectSelect.options[subjectSelect.selectedIndex].text,
-                                description: detailSelect.options[detailSelect.selectedIndex].text,
-                                latitude: latlng.lat,
-                                longitude: latlng.lng,
-                                timestamp: new Date().toISOString()
-                            };
-                            fetch('/api/incidents', {
-                                method: 'POST',
-                                headers: { 'Content-Type': 'application/json' },
-                                body: JSON.stringify(incidentData)
-                            })
-                            .then(res => res.json())
-                            .then(data => {
-                            })
-                            .catch(err => {
-                            });
+                        // Envoie les données de l'incident au backend
+                        var incidentData = {
+                            type: subjectSelect.options[subjectSelect.selectedIndex].text,
+                            description: detailSelect.options[detailSelect.selectedIndex].text,
+                            latitude: latlng.lat,
+                            longitude: latlng.lng,
+                            timestamp: new Date().toISOString()
+                        };
+                        fetch('/api/incidents', {
+                            method: 'POST',
+                            headers: { 'Content-Type': 'application/json' },
+                            body: JSON.stringify(incidentData)
+                        })
+                        .then(res => res.json())
+                        .then(data => {
+                        })
+                        .catch(err => {
+                        });
                     };
                 }
             }, 100);
